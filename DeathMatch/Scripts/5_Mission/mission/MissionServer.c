94f46722444265276de5dfb8e70693bc136417f2
Ben Potter <benpotterlikesmath@gmail.com>
2025-06-14 00:52:22 -0400

    added back async calls

--- DeathMatch/Scripts/5_Mission/mission/MissionServer.c
+++ DeathMatch/Scripts/5_Mission/mission/MissionServer.c
@@ -424,11 +424,7 @@ modded class MissionServer
 		
 		player.m_dmPlayerData.m_CurrentWeapon = wpnData.m_Id;
 		player.SynchDmPlayerDataDirty();
-
-		// dont do async call to try and fix desync issue
-		//GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(EquipPlayer_DM, 1000, false, player);
-
-		EquipPlayer_DM(player);
+		GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(EquipPlayer_DM, 1000, false, player);
 	}
 	
 	void DM_EquipmentBuy(CallType type, ParamsReadContext ctx, PlayerIdentity sender, Object target)
